6d02f0a7558b27596450f9ec72d4905f8362fc25
Joursoir <chat@joursoir.net>
2021-03-29 17:21:15 +0000

    add Vertex struct

---
+++ src/graphics/Vertex.hpp
@@ -0,0 +1,11 @@
+#ifndef ENGINE_VERTEX_H
+#define ENGINE_VERTEX_H
+
+#include <glm/glm.hpp>
+
+typedef struct {
+	glm::vec3 pos_coords;
+	glm::vec2 tex_coords;
+} Vertex;
+
+#endif /* ENGINE_VERTEX_H */
