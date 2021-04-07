4f3a08c3c5ffe451e48340205ba5ba866828eb17
Joursoir <chat@joursoir.net>
2021-04-07 21:26:48 +0000

    xstdlib: add template for linked list

--- src/xstdlib/xstdlib.hpp
+++ src/xstdlib/xstdlib.hpp
@@ -1,6 +1,15 @@
 #ifndef ENGINE_XSTDLIB_H
 #define ENGINE_XSTDLIB_H
 
+template <class T>
+struct linked_list {
+	T *data;
+	struct linked_list *next;
+
+	linked_list(T *a_data, struct linked_list *a_next)
+		: data(a_data), next(a_next) { }
+};
+
 char *xfread(const char *path, const char *mode);
 
 #endif /* ENGINE_XSTDLIB_H */
